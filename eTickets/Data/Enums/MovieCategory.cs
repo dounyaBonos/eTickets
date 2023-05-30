dbc4f0a99e46eb16781e651be076aceb9bcf5082
d.elhayanimoustamid <d.elhayanimoustamid@E6K16-VDI103>
2023-05-30 10:14:34 +0200

    Ajoutez des fichiers projet.

---
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Drama,
+        Comedy,
+        Action,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name ="Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "FullName")]
+
+        public string FullName { get; set; }
+      
+        [Display(Name = "Biography")]
+
+        public string Bio { get; set; }
+
+        //RelationShips
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+    }
+}
